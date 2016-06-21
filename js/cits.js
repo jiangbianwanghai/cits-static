bc87ad633cce87f506707cf7af753254dfca320d
jiangbianwanghai <webmaster@jiangbianwanghai.com>
2016-06-21 21:21:26 +0800

    实现提醒功能

--- js/cits.js
+++ js/cits.js
@@ -24,6 +24,26 @@ jQuery(document).ready(function() {
       }
   });
 
+  //读取提醒
+  setTimeout(function () {
+    $.ajax({
+      type: "GET",
+      url: "/dashboard/get_notify",
+      dataType: "JSON",
+      success: function(data){
+        if (data.total) {
+          blink('#bell');
+          $("#notify-total").append('<span class="badge">'+data.total+'</span>');
+          var notify_list = '';
+          for(var p in data.datas){
+            notify_list += '<li class="new"><span class="thumb"><img src="http://avatar.cits.gongchang.net/'+data.datas[p].log_sender_username+'.jpg" alt="发送者：'+data.datas[p].log_sender_realname+'" /></span><span class="desc"><span class="name">'+data.datas[p].log_sender_realname+' <span class="badge badge-success">未读</span></span><span class="msg"><a href="'+data.datas[p].log_url+'">'+data.datas[p].log_action+'了一个'+data.datas[p].log_target_type+'#'+data.datas[p].log_content+'</a></span></span></li>';
+          }
+          $("#notify-content").append('<div class="dropdown-menu dropdown-menu-head pull-right"><h5 class="title">你有'+data.total+'个新提醒</h5><ul class="dropdown-list gen-list">'+notify_list+'<li class="new"><a href="">查看所有通知</a></li></ul></div>');
+        }
+      }
+    });
+  }, 0);
+
 });
 
 //验证项目表单
@@ -57,4 +77,12 @@ function callBackProject(data) {
       location.href = window.location.href;
     }, 3000);
   }
+}
+
+function blink(selector){
+  $(selector).fadeOut('slow', function(){
+    $(this).fadeIn('slow', function(){
+      blink(this);
+    });
+  });
 }
